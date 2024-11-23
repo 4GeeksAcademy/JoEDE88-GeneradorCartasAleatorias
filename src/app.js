e15a5f3af63e9a2f5275ec262e48548c7ff93163
Joe <joediaz88@hotmail.com>
2024-11-23 20:10:26 +0000

    pulido el código para el proyecto lineamentos de código

--- src/app.js
+++ src/app.js
@@ -1,34 +1,33 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 import "./assets/img/King.png";
 import "./assets/img/queen.png";
 import "./assets/img/jack.png";
 
+let card = document.getElementById("randomCard");
 let suit = document.getElementById("idPica");
 let numberOrLetter = document.getElementById("numeroOLetra");
 let reversedSuit = document.getElementById("picaReves");
+let boton = document.getElementById("btn");
+let widthInput = document.getElementById("inputWidth");
+let heightInput = document.getElementById("inputHeight");
 
-let suits = ["♠", "♣", "♥", "♦"];
-let numerosYLetras = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
-
-function cartaAleatoria() {
+function generarCartaAleatoria() {
+  let suits = ["♠", "♣", "♥", "♦"];
+  let numerosYLetras = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
   let randomSuit = suits[Math.floor(Math.random() * suits.length)];
   let randomNumber =
     numerosYLetras[Math.floor(Math.random() * numerosYLetras.length)];
   suit.innerHTML = randomSuit;
   numberOrLetter.innerHTML = randomNumber;
   reversedSuit.innerText = suit.innerText;
-  if (randomSuit == "♥" || randomSuit == "♦") {
-    suit.style.color = "red";
-    reversedSuit.style.color = "red";
-  } else {
-    suit.style.color = "black";
-    reversedSuit.style.color = "black";
-  }
+
+  suit.style.color = reversedSuit.style.color =
+    randomSuit == "♥" || randomSuit == "♦" ? "red" : "black";
+
   if (randomNumber === "K") {
     numberOrLetter.innerHTML = `<img id="myImg" src="King.png"></img>`;
   }
@@ -40,29 +39,23 @@ function cartaAleatoria() {
   }
 }
 
-window.onload = function() {
-  cartaAleatoria();
-};
-
-let card = document.getElementById("randomCard");
-
-card.addEventListener("click", cartaAleatoria);
-
-let boton = document.getElementById("btn");
-boton.addEventListener("click", cartaAleatoria);
-
-const tresSegundos = 3000;
-setInterval(cartaAleatoria, tresSegundos);
-
-let widthInput = document.getElementById("inputWidth");
-let heightInput = document.getElementById("inputHeight");
-
 function adjustWidth(size) {
   card.style.width = `${size}px`;
 }
 function adjustHeight(size) {
   card.style.height = `${size}px`;
 }
+
+window.onload = function() {
+  generarCartaAleatoria();
+};
+
+card.addEventListener("click", generarCartaAleatoria);
+
+boton.addEventListener("click", generarCartaAleatoria);
+
+const tresSegundos = 3000;
+setInterval(generarCartaAleatoria, tresSegundos);
 
 widthInput.addEventListener("keypress", function(e) {
   if (e.key === "Enter" && e.target.value) {
